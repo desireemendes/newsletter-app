b312cf77c3910d2081c382155c9b33fa3859a11b
Desiree Mendes <desireefrost@live.ca>
2022-02-11 12:51:21 -0500

    post for form submission

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const request = require("request")
 const path = require('path');
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/signup.html'));
 });
+
+app.post('/', function (req, res) {
+const firstName = req.body.fName
+const lastName = req.body.lName
+const email = req.body.email
+console.log(firstName, lastName, email)
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App listening on ${PORT}...`);
